c527179caf5fdbf60119631d672a49e681848831
AmanitaJT <66037169+Mikah1998@users.noreply.github.com>
2025-08-27 00:24:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
+const jukeboxData = [
+  { img: "ykw1boxart.webp", audio: "videoplayback.m4a" },
+  { img: "psychicspectersboxart.webp", audio: "ykw2.mp3" },
+  { img: "whiteboxart.webp", audio: "ykwblasters.m4a" },
+  { img: "ykw3boxart.webp", audio: "ykw3.m4a" },
+  { img: "4++boxart.webp", audio: "ykw4.m4a" },
+  { img: "yschoolboxart.webp", audio: "yschool.m4a "}
+];
+
+let musicaAtual = 0;
+
 function changeImage() {
     const img = document.getElementById("imgClickAndChange");
     const audio = document.querySelector("audio"); 
     const source = document.getElementById("jukeboxmusica");
 
 
-    if (img.src.includes("ykw1boxart.webp")){
-        img.src = "whiteboxart.webp";
-        source.src = "ykwblasters.m4a";
-    } else {
-        img.src = "ykw1boxart.webp";
-        source.src = "videoplayback.m4a";
+    musicaAtual++;
+      if (musicaAtual >= jukeboxData.length) {
+      musicaAtual = 0; 
     }
+
+  img.src = jukeboxData[musicaAtual].img;
+  source.src = jukeboxData[musicaAtual].audio;
+
 
     audio.load();
     audio.play();
 }
+
 
 
 
