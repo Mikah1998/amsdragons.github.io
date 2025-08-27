3e2cf27ea02666fc5d27097a83cbdeb6fcc1abdf
AmanitaJT <66037169+Mikah1998@users.noreply.github.com>
2025-08-26 23:23:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function changeImage() {
     const source = document.getElementById("jukeboxmusica");
 
 
-    if (document.getElementById("imgClickAndChange").src == "https://static.wikia.nocookie.net/yokaiwatch/images/f/f0/Yokai-watch-boxart.png/revision/latest/scale-to-width-down/1000?cb=20160623131935"){
-        img.src = "https://static.wikia.nocookie.net/yokaiwatch/images/5/5b/YWBlastersRCCEuropeanEnglishBoxart.png/revision/latest/scale-to-width-down/1000?cb=20180731164507";
+    if (document.getElementById("imgClickAndChange").src == "ykw1boxart.webp"){
+        img.src = "redboxart.webp";
         source.src = "ykwblasters.m4a";
     } else {
-        img.src = "https://static.wikia.nocookie.net/yokaiwatch/images/f/f0/Yokai-watch-boxart.png/revision/latest/scale-to-width-down/1000?cb=20160623131935";
+        img.src = "ykw1boxart.webp";
         source.src = "videoplayback.m4a";
     }
 
     audio.load();
     audio.play();
 }
+
